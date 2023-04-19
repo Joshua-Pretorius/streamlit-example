6414d9f5341855348322ef689ac1267e73277757
Joshua-Pretorius <131198616+Joshua-Pretorius@users.noreply.github.com>
2023-04-19 14:09:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,38 @@ fig1 = px.bar(airline_counts, x="Country", y="Count", color="Country", title="Ai
 st.plotly_chart(fig1, theme = 'streamlit')
 
 
+## MAP TRY 1
+import streamlit as st
+import pandas as pd
+import numpy as np
+import folium
+
+# Load airports and routes data
+airports = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat',
+                          header=None, names=['airport_id', 'name', 'city', 'country', 'iata', 'icao', 'latitude', 'longitude', 'altitude', 'timezone', 'dst', 'tz'])
+routes = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat',
+                        header=None, names=['airline', 'airline_id', 'source', 'source_id', 'dest', 'dest_id', 'codeshare', 'stops', 'equipment'])
+
+# Merge airport data to get latitude and longitude for each airport
+routes = pd.merge(routes, airports[['iata', 'latitude', 'longitude']], left_on='source', right_on='iata', how='left', suffixes=('_source', '_dest'))
+routes = pd.merge(routes, airports[['iata', 'latitude', 'longitude']], left_on='dest', right_on='iata', how='left', suffixes=('_source', '_dest'))
+
+# Create a list of unique airline names for dropdown menu
+airline_names = sorted(routes['airline'].unique())
+
+# Create a map centered at 0, 0 coordinates
+m = folium.Map(location=[0, 0], zoom_start=2)
+
+# Add markers for each airport
+for _, row in airports.iterrows():
+    folium.Marker(location=[row['latitude'], row['longitude']], popup=row['name']).add_to(m)
+
+# Add flight routes for the selected airline
+selected_airline = st.sidebar.selectbox('Select airline', airline_names)
+selected_routes = routes[routes['airline'] == selected_airline]
+
+for _, row in selected_routes.iterrows():
+    folium.PolyLine(locations=[[row['latitude_source'], row['longitude_source']], [row['latitude_dest'], row['longitude_dest']]], color='blue').add_to(m)
+
+# Display the map
+st.markdown(folium.Map().get_root().render(), unsafe_allow_html=True)
