601178fc829ac9369173ff3174899ffae3847395
Joshua-Pretorius <131198616+Joshua-Pretorius@users.noreply.github.com>
2023-04-19 13:55:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 import plotly.express as px
 import folium
 from folium.plugins import MarkerCluster
@@ -10,39 +10,31 @@ airports_df = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflight
 routes_df = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat',
                         header=None, names=['airline', 'airline_id', 'source', 'source_id', 'dest', 'dest_id', 'codeshare', 'stops', 'equipment'])
 
-# Set up the layout
-st.set_page_config(page_title="Airports", page_icon=":airplane:", layout="wide")
-st.title("Airports")
+# Create the app
+st.title("Airport Dashboard")
 
-# Create the sidebar
-st.sidebar.title("Select an option:")
-option = st.sidebar.selectbox("", ["Number of airports in each country", "Number of routes per airline", "Number of routes per source airport"])
+# Create figure 1: Number of airports in each country
+airports_per_country = airports_df.groupby('country').size().reset_index(name='count')
+fig1 = px.bar(airports_per_country, x='country', y='count', title='Number of airports in each country')
 
-# Define the main panel
-st.write("## " + option)
+# Create figure 2: Number of routes per airline
+routes_per_airline = routes_df.groupby('airline').size().reset_index(name='count')
+fig2 = px.bar(routes_per_airline, x='airline', y='count', title='Number of routes per airline')
 
-# Create the figures
-if option == "Number of airports in each country":
-    airports_per_country = airports_df.groupby('country').size().reset_index(name='count')
-    fig = px.bar(airports_per_country, x='country', y='count', title='Number of airports in each country')
-    st.plotly_chart(fig, use_container_width=True)
-
-elif option == "Number of routes per airline":
-    routes_per_airline = routes_df.groupby('airline').size().reset_index(name='count')
-    fig = px.bar(routes_per_airline, x='airline', y='count', title='Number of routes per airline')
-    st.plotly_chart(fig, use_container_width=True)
-
-else:
-    routes_per_source_airport = routes_df.groupby('source').size().reset_index(name='count')
-    fig = px.bar(routes_per_source_airport, x='source', y='count', title='Number of routes per source airport')
-    st.plotly_chart(fig, use_container_width=True)
+# Create figure 3: Number of routes per source airport
+routes_per_source_airport = routes_df.groupby('source').size().reset_index(name='count')
+fig3 = px.bar(routes_per_source_airport, x='source', y='count', title='Number of routes per source airport')
 
 # Create the map
-st.write("## Map of Airports")
-m = folium.Map(location=[30, 0], zoom_start=2)
-marker_cluster = MarkerCluster().add_to(m)
+st.subheader("Airports around the world")
+marker_cluster = MarkerCluster().add_to(folium.Map(location=[0, 0], zoom_start=2))
 for _, row in airports_df.iterrows():
     folium.Marker(location=[row['latitude'], row['longitude']], popup=row['name']).add_to(marker_cluster)
-st.markdown(folium_static(m))
+
+# Render the figures and map in the app
+st.plotly_chart(fig1)
+st.plotly_chart(fig2)
+st.plotly_chart(fig3)
+st_folium_map(marker_cluster)
 
 
