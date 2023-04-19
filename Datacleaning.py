bab6aa72e4d877535af799d7338bd031a6b996bf
Joshua-Pretorius <131198616+Joshua-Pretorius@users.noreply.github.com>
2023-04-19 23:12:44 +0200

    Create Datacleaning.py

---
+++ Datacleaning.py
@@ -0,0 +1,20 @@
+import pandas as pd
+airlines = pd.read_csv('airlines.dat', header=None)
+airline_col = ['Airline ID', 'Name', 'Alias', 'IATA', 'ICAO', 'Callsign', 'Country', 'Active']
+airlines.columns = airline_col
+
+airports = pd.read_csv('airports.dat', header=None)
+airport_col = ['Airport ID', 'Name', 'City', 'Country', 'IATA', 'ICAO', 'Latitude', 'Longitude', 'Altitude', 'Timezone', 'DST', 'Tz database time zone', 'Type', 'Source']
+airports.columns = airport_col
+
+countries = pd.read_csv('countries.dat', header=None)
+country_col = ['Name', 'ISO Code', 'DAFIF Code']
+countries.columns = country_col
+
+planes = pd.read_csv('planes.dat', header=None)
+plane_col = ['Name', 'IATA code', 'ICAO code']
+planes.columns = plane_col
+
+routes = pd.read_csv('routes.dat', header=None)
+route_col = ['Airline', 'Airline ID', 'Source airport', 'Source airport ID', 'Destination airport', 'Destination airport ID', 'Codeshare', 'Stops', 'Equipment']
+routes.columns = route_col
