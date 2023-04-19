c2ec402bf42e02e498436577dc8de3fc51d134e8
Joshua-Pretorius <131198616+Joshua-Pretorius@users.noreply.github.com>
2023-04-19 13:48:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,48 @@
-from collections import namedtuple
-import altair as alt
-import math
+import streamlit as st
 import pandas as pd
 import plotly.express as px
-import streamlit as st
+import folium
+from folium.plugins import MarkerCluster
 
-airlines = pd.read_csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airlines.dat",
-                       header=None, names=["Airline ID", "Name", "Alias", "IATA", "ICAO", "Callsign", "Country", "Active"])
-airports = pd.read_csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat",
-                       header=None, names=["Airport ID", "Name", "City", "Country", "IATA", "ICAO", "Latitude", "Longitude", "Altitude", "Timezone", "DST", "Tz database time zone"])
-routes = pd.read_csv("https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat",
-                     header=None, names=["Airline", "Airline ID", "Source airport", "Source airport ID", "Destination airport", "Destination airport ID", "Codeshare", "Stops", "Equipment"])
+# Load datasets
+airports_df = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat',
+                          header=None, names=['airport_id', 'name', 'city', 'country', 'iata', 'icao', 'latitude', 'longitude', 'altitude', 'timezone', 'dst', 'tz'])
+routes_df = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat',
+                        header=None, names=['airline', 'airline_id', 'source', 'source_id', 'dest', 'dest_id', 'codeshare', 'stops', 'equipment'])
 
-# create the web application using streamlit
-st.set_page_config(page_title="OpenFlights Dashboard", page_icon="✈️", layout="wide")
-st.title("OpenFlights Dashboard")
+# Set up the layout
+st.set_page_config(page_title="Airports", page_icon=":airplane:", layout="wide")
+st.title("Airports")
 
-st.header("Airline Data")
-st.write(airlines)
-st.subheader("Airline Count by Country")
-airline_counts = airlines.groupby("Country").size().reset_index(name="Count")
-fig1 = px.bar(airline_counts, x="Country", y="Count", color="Country", title="Airline Count by Country")
-st.plotly_chart(fig1, theme = 'streamlit')
+# Create the sidebar
+st.sidebar.title("Select an option:")
+option = st.sidebar.selectbox("", ["Number of airports in each country", "Number of routes per airline", "Number of routes per source airport"])
+
+# Define the main panel
+st.write("## " + option)
+
+# Create the figures
+if option == "Number of airports in each country":
+    airports_per_country = airports_df.groupby('country').size().reset_index(name='count')
+    fig = px.bar(airports_per_country, x='country', y='count', title='Number of airports in each country')
+    st.plotly_chart(fig, use_container_width=True)
+
+elif option == "Number of routes per airline":
+    routes_per_airline = routes_df.groupby('airline').size().reset_index(name='count')
+    fig = px.bar(routes_per_airline, x='airline', y='count', title='Number of routes per airline')
+    st.plotly_chart(fig, use_container_width=True)
+
+else:
+    routes_per_source_airport = routes_df.groupby('source').size().reset_index(name='count')
+    fig = px.bar(routes_per_source_airport, x='source', y='count', title='Number of routes per source airport')
+    st.plotly_chart(fig, use_container_width=True)
+
+# Create the map
+st.write("## Map of Airports")
+m = folium.Map(location=[30, 0], zoom_start=2)
+marker_cluster = MarkerCluster().add_to(m)
+for _, row in airports_df.iterrows():
+    folium.Marker(location=[row['latitude'], row['longitude']], popup=row['name']).add_to(marker_cluster)
+st.markdown(folium_static(m))
+
+
