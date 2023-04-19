0a774c6478bd4c02e320a6e32471cd2b7cee0f6c
Joshua-Pretorius <131198616+Joshua-Pretorius@users.noreply.github.com>
2023-04-19 16:06:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ airline_counts = airlines.groupby("Country").size().reset_index(name="Count")
 fig1 = px.bar(airline_counts, x="Country", y="Count", color="Country", title="Airline Count by Country")
 st.plotly_chart(fig1, theme = 'streamlit')
 
-##MAP1
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -31,17 +30,22 @@ import folium
 
 # Load airports and routes data
 airports = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat',
-                          header=None, names=['airport_id', 'name', 'city', 'country', 'iata', 'icao', 'latitude', 'longitude', 'altitude', 'timezone', 'dst', 'tz'])
+                       header=None,
+                       names=['airport_id', 'name', 'city', 'country', 'iata', 'icao', 'latitude', 'longitude', 'altitude', 'timezone', 'dst', 'tz'])
+
 routes = pd.read_csv('https://raw.githubusercontent.com/jpatokal/openflights/master/data/routes.dat',
-                        header=None, names=['airline', 'airline_id', 'source', 'source_id', 'dest', 'dest_id', 'codeshare', 'stops', 'equipment'])
+                     header=None,
+                     names=['airline', 'airline_id', 'source', 'source_id', 'dest', 'dest_id', 'codeshare', 'stops', 'equipment'])
 
 # Merge airport data to get latitude and longitude for each airport
-airports = airports[['iata', 'latitude', 'longitude']].dropna()
-airports['latitude'] = airports['latitude'].astype(float)
-airports['longitude'] = airports['longitude'].astype(float)
+source_airports = airports[['iata', 'latitude', 'longitude']]
+dest_airports = airports[['iata', 'latitude', 'longitude']]
+routes = pd.concat([pd.merge(routes[['airline', 'source', 'dest']], source_airports, left_on='source', right_on='iata', how='left'), 
+                    pd.merge(routes[['airline', 'source', 'dest']], dest_airports, left_on='dest', right_on='iata', how='left')], 
+                   ignore_index=True)
 
-routes = pd.merge(routes, airports, left_on='source', right_on='iata', how='left', suffixes=('_source', '_dest')).dropna()
-routes = pd.merge(routes, airports, left_on='dest', right_on='iata', how='left', suffixes=('_source', '_dest')).dropna()
+# Drop null values
+routes = routes.dropna(subset=['latitude', 'longitude'])
 
 # Create a list of unique airline names for dropdown menu
 airline_names = sorted(routes['airline'].unique())
@@ -58,9 +62,10 @@ selected_airline = st.sidebar.selectbox('Select airline', airline_names)
 selected_routes = routes[routes['airline'] == selected_airline]
 
 for _, row in selected_routes.iterrows():
-    folium.PolyLine(locations=[[row['latitude_source'], row['longitude_source']], [row['latitude_dest'], row['longitude_dest']]], color='blue').add_to(m)
+    folium.PolyLine(locations=[[row['latitude'][0], row['longitude'][0]], [row['latitude'][1], row['longitude'][1]]], color='blue').add_to(m)
 
 # Display the map
 st.markdown(folium.Map().get_root().render(), unsafe_allow_html=True)
+
 
 
